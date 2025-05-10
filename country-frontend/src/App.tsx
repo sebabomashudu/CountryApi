92dba39745b48d8c43c9835a0b2188409154c7fd
sebabomashudu <130669349+sebabomashudu@users.noreply.github.com>
2025-05-10 20:30:30 +0200

    Update App.tsx

--- country-frontend/src/App.tsx
+++ country-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import DetailPage from './pages/DetailPage'
-//testing
+
 function App() {
   return (
     <BrowserRouter>
